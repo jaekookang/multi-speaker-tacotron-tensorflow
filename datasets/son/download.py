b7e5ea8e901df528220e488f49b840eae6ca488c
jaekookang <jkang@gradcenter.cuny.edu>
2018-01-05 02:46:25 +0900

    update code, transcribe wav

--- datasets/son/download.py
+++ datasets/son/download.py
@@ -13,8 +13,10 @@ from utils import get_encoder_name, parallel_run, makedirs
 API_URL = 'http://api.jtbc.joins.com/ad/pre/NV10173083'
 BASE_URL = 'http://nsvc.jtbc.joins.com/API/News/Newapp/Default.aspx'
 
+
 def soupify(text):
     return BeautifulSoup(text, "html.parser")
+
 
 def get_news_ids(page_id):
     params = {
@@ -30,8 +32,9 @@ def get_news_ids(page_id):
 
     return [item.text for item in soup.find_all('news_id')]
 
+
 def download_news_video_and_content(
-        news_id, base_dir, chunk_size=32*1024,
+        news_id, base_dir, chunk_size=32 * 1024,
         video_dir="video", asset_dir="assets", audio_dir="audio"):
 
     video_dir = os.path.join(base_dir, video_dir)
@@ -43,14 +46,15 @@ def download_news_video_and_content(
     makedirs(audio_dir)
 
     text_path = os.path.join(asset_dir, "{}.txt".format(news_id))
-    original_text_path = os.path.join(asset_dir, "original-{}.txt".format(news_id))
+    original_text_path = os.path.join(
+        asset_dir, "original-{}.txt".format(news_id))
 
     video_path = os.path.join(video_dir, "{}.ts".format(news_id))
     audio_path = os.path.join(audio_dir, "{}.wav".format(news_id))
 
     params = {
         'NJC': 'NJC400',
-        'NID': news_id, # NB11515152
+        'NID': news_id,  # NB11515152
         'CD': 'A0100',
     }
 
@@ -62,9 +66,10 @@ def download_news_video_and_content(
     article_contents = soup.find_all('article_contents')
 
     assert len(article_contents) == 1, \
-            "# of <article_contents> of {} should be 1: {}".format(news_id, response.text)
+        "# of <article_contents> of {} should be 1: {}".format(
+            news_id, response.text)
 
-    text = soupify(article_contents[0].text).get_text() # remove <div>
+    text = soupify(article_contents[0].text).get_text()  # remove <div>
 
     with open(original_text_path, "w") as f:
         f.write(text)
@@ -72,12 +77,13 @@ def download_news_video_and_content(
     with open(text_path, "w") as f:
         from nltk import sent_tokenize
 
-        text = re.sub(r'\[.{0,80} :\s.+]', '', text) # remove quote
-        text = re.sub(r'☞.+http.+\)', '', text) # remove quote
-        text = re.sub(r'\(https?:\/\/.*[\r\n]*\)', '', text) # remove url
+        text = re.sub(r'\[.{0,80} :\s.+]', '', text)  # remove quote
+        text = re.sub(r'☞.+http.+\)', '', text)  # remove quote
+        text = re.sub(r'\(https?:\/\/.*[\r\n]*\)', '', text)  # remove url
 
         sentences = sent_tokenize(text)
-        sentences = [sent for sentence in sentences for sent in sentence.split('\n') if sent]
+        sentences = [
+            sent for sentence in sentences for sent in sentence.split('\n') if sent]
 
         new_texts = []
         for sent in sentences:
@@ -92,13 +98,14 @@ def download_news_video_and_content(
     vod_paths = soup.find_all('vod_path')
 
     assert len(vod_paths) == 1, \
-            "# of <vod_path> of {} should be 1: {}".format(news_id, response.text)
+        "# of <vod_path> of {} should be 1: {}".format(news_id, response.text)
 
     if not os.path.exists(video_path):
         redirect_url = soup.find_all('vod_path')[0].text
 
         list_url = m3u8.load(redirect_url).playlists[0].absolute_uri
-        video_urls = [segment.absolute_uri for segment in m3u8.load(list_url).segments]
+        video_urls = [
+            segment.absolute_uri for segment in m3u8.load(list_url).segments]
 
         with open(video_path, "wb") as f:
             for url in video_urls:
@@ -106,16 +113,17 @@ def download_news_video_and_content(
                 total_size = int(response.headers.get('content-length', 0))
 
                 for chunk in response.iter_content(chunk_size):
-                    if chunk: # filter out keep-alive new chunks
+                    if chunk:  # filter out keep-alive new chunks
                         f.write(chunk)
 
     if not os.path.exists(audio_path):
         encoder = get_encoder_name()
         command = "{} -y -loglevel panic -i {} -ab 160k -ac 2 -ar 44100 -vn {}".\
-                format(encoder, video_path, audio_path)
+            format(encoder, video_path, audio_path)
         subprocess.call(command, shell=True)
 
     return True
+
 
 if __name__ == '__main__':
     news_ids = []
@@ -131,7 +139,8 @@ if __name__ == '__main__':
                 break
 
             news_ids.extend(tmp_ids)
-            print(" [*] Download page {}: {}/{}".format(page_idx, len(tmp_ids), len(news_ids)))
+            print(" [*] Download page {}: {}/{}".format(page_idx,
+                                                        len(tmp_ids), len(news_ids)))
 
             page_idx += 1
 
@@ -147,4 +156,4 @@ if __name__ == '__main__':
     fn = partial(download_news_video_and_content, base_dir=base_dir)
 
     results = parallel_run(
-            fn, news_ids, desc="Download news video+text", parallel=True)
+        fn, news_ids, desc="Download news video+text", parallel=True)
--- scripts/prepare_jtbc.sh
+++ scripts/prepare_jtbc.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # 1. Download and extract audio and texts
-python -m datasets.jtbc.download
+python -m datasets.son.download
 
 # 2. Split audios on silence
-python -m audio.silence --audio_pattern "./datasets/jtbc/audio/*.wav" --method=pydub
+python -m audio.silence --audio_pattern "./datasets/son/audio/*.wav" --method=pydub
 
 # 3. Run Google Speech Recognition
-python -m recognition.google --audio_pattern "./datasets/jtbc/audio/*.*.wav"
+export GOOGLE_APPLICATION_CREDENTIALS="My First Project-1df1d151da57.json"
+python -m recognition.google --audio_pattern "./datasets/son/audio/*.*.wav"
 
 # 4. Run heuristic text-audio pair search (any improvement on this is welcome)
-python -m recognition.alignment --recognition_path "./datasets/jtbc/recognition.json" --score_threshold=0.5
+python -m recognition.alignment --recognition_path "./datasets/son/recognition.json" --score_threshold=0.5
 
 # 5. Remove intro music
-rm datasets/jtbc/data/*.0000.npz
+rm datasets/son/data/*.0000.npz
